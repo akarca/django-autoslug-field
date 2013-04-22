7efeb3f23b7adaa03a6e153d6b87271686bcda7a
Serdar Akarca <serdar@yuix.org>
2013-04-22 10:48:16 +0300

    update version

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'django-autoslug-field',
-    version = '0.2.2',
-    description = "AutoSlugField for Django based on django-extensions AutoSlugField, adds option to track foreignkey/parent field for slug.",
-    long_description = open('README.rst').read(),
-    author = 'Aljosa Mohorovic',
-    author_email = 'aljosa.mohorovic@gmail.com',
-    url='http://github.com/aljosa/django-autoslug-field/',
-    packages = find_packages(),
-    include_package_data = True,
-    license = "MIT License",
-    keywords = "django autoslug slug field",
-    platforms = ['any'],
+    name='django-autoslug-field',
+    version='0.2.3',
+    description="AutoSlugField for Django based on django-extensions AutoSlugField, adds option to track foreignkey/parent field for slug.",
+    long_description=open('README.rst').read(),
+    author='Serdar Akarca',
+    author_email='serdar@yuix.org',
+    url='http://github.com/serdarakarca/django-categories',
+    packages=find_packages(),
+    include_package_data=True,
+    license="MIT License",
+    keywords="django autoslug slug field",
+    platforms=['any'],
 )
